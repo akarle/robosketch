160a93f5c52a9e8a5adf3f139accbf9c9f22643b
Alex Guerriero <aguerriero@umass.edu>
2018-03-31 14:31:47 -0400

    Editted CMake, Initalized Files

---
+++ robosketch/src/DepthImage2PC.cpp
@@ -0,0 +1,1 @@
+#include "DepthImage2PC.h"
---
+++ robosketch/src/DepthImage2PC.h
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <cmath>
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <sstream>
+#include <limits>
+
+#include <eigen3/Eigen/Dense>
+#include <geometry_msgs/Point.h>
+#include <nav_msgs/Odometry.h>
