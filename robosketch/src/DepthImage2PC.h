5f1c12f2826d3ab1efeab3ab2624bac8182f6e6e
Alex Guerriero <aguerriero@umass.edu>
2018-04-01 13:54:39 -0400

    Depth Image to PointCloud

--- robosketch/src/DepthImage2PC.h
+++ robosketch/src/DepthImage2PC.h
@@ -7,7 +7,21 @@
 #include <std_msgs/String.h>
 #include <sstream>
 #include <limits>
+#include <cstdlib>
 
 #include <eigen3/Eigen/Dense>
 #include <geometry_msgs/Point.h>
-#include <nav_msgs/Odometry.h>
+
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/PointCloud.h>
+
+using std::vector;
+using sensor_msgs::Image;
+using geometry_msgs::Point;
+using sensor_msgs::PointCloud;
+using Eigen::Vector3f;
+using Eigen::Matrix;
+
+Point getPointFromDisparity(double disparity, double x, double y);
+void imageToPointCloud(Image &image);
+PointCloud RANSAC(const PointCloud);
