63cd649b6b53e0256e5ad2ac62edafd7e51f6e47
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2018-05-02 16:27:18 -0400

    Update VelPublisher.cpp

--- robosketch/src/VelPublisher.cpp
+++ robosketch/src/VelPublisher.cpp
@@ -8,8 +8,8 @@ int main(int argc, char** argv) {
 
 	ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/robosketch/commands",1000);
 	geometry_msgs::Twist test;
-	test.linear.x=5;
-	test.angular.z=2;
+	test.linear.x=.5;
+	test.angular.z=0.01;
 	ros::Rate rate(30);
 	while(true) {
 		pub.publish(test);
